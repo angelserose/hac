2f744777974f71a8ca795aa84a1e9412404ac6a4
Arsha006 <arsharajeshbabum@gmail.com>
2025-02-01 18:27:21 +0530

    update

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// Slider logic
+let currentSlide = 0;
+
+function moveSlide(direction) {
+    const slides = document.querySelectorAll('.slide');
+    const totalSlides = slides.length;
+    currentSlide = (currentSlide + direction + totalSlides) % totalSlides;
+    document.querySelector('.slider-container').style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+// Popup logic
 function showPopup(id) {
     document.getElementById(id).style.display = "block";
 }
